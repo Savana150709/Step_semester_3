7e73e6eb124272a43cf5b9a5fad110f509fd9db0
Savana <aj8867@srmist.edu.in>
2026-09-04 22:11:04 +0530

    Add session 2 string operation & performance class problems

---
+++ src/stringoperationsandperformance/class_problems/CSVStudentRecordParser.java
@@ -0,0 +1,24 @@
+package stringoperationsandperformance.class_problems;
+
+public class CSVStudentRecordParser {
+
+    static void parseStudentRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+        } else {
+            System.out.println("Name: " + fields[0]
+                    + " | Roll No: " + fields[1]
+                    + " | Dept: " + fields[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String record = "Ananya Verma,RA2211003010123,CSE";
+
+        parseStudentRecord(record);
+    }
+}
