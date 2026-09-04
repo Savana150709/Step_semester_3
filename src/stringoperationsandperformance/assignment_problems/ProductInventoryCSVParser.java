01804fb60f0a118639ac76dd45d904e98bd9b54d
Savana <aj8867@srmist.edu.in>
2026-09-04 22:27:20 +0530

    Add session 2 string operation & performance assignment problems

---
+++ src/stringoperationsandperformance/assignment_problems/ProductInventoryCSVParser.java
@@ -0,0 +1,24 @@
+package stringoperationsandperformance.assignment_problems;
+
+public class ProductInventoryCSVParser {
+
+    static void parseInventoryRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+        } else {
+            System.out.println("Product: " + fields[0]
+                    + " | SKU: " + fields[1]
+                    + " | Qty: " + fields[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String record = "Wireless Mouse,WM-2201,150";
+
+        parseInventoryRecord(record);
+    }
+}
