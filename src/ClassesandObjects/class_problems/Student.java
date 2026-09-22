a8a0d573a42bde547aecbc0b6aebdd8856351e7b
Savana <aj8867@srmist.edu.in>
2026-09-22 19:30:02 +0530

    Added session 6 class problems

---
+++ src/ClassesandObjects/class_problems/Student.java
@@ -0,0 +1,27 @@
+package ClassesandObjects.class_problems;
+
+public class Student {
+    String name;
+    double attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    public static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Ravi", 85.0);
+        Student s2 = new Student("Anitha", 92.5);
+
+        Student.printCollegeInfo();
+    }
+}
